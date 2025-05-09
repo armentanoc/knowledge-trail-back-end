d2b108f011da8e90894034a76d3d3990667b3a16
armentanoc <armentanocarolina@gmail.com>
2025-05-09 11:43:27 -0300

    #time 1m - fix pageable in employee repository.

--- src/main/java/br/ucsal/infrastructure/IEmployeeRepository.java
+++ src/main/java/br/ucsal/infrastructure/IEmployeeRepository.java
@@ -3,10 +3,11 @@ package br.ucsal.infrastructure;
 import br.ucsal.domain.users.Employee;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
-    Page<Employee> findAll(Employee pageable);  
+    Page<Employee> findAll(Pageable pageable);  
 }
